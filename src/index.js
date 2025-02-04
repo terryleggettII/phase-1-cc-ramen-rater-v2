a504bf4edd733f95988588319844d174a554f5bf
Terry Leggett II <terryleggettjr@gmail.com>
2025-02-03 23:44:42 -0500

    completed code challenge

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
-// index.js
+const API = "http://localhost:3000/ramens"
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+el('new-ramen').addEventListener('submit', createNewRamen)
 
-const addSubmitListener = () => {
-  // Add code
+fetch(API)
+  .then((res) => res.json())
+  .then(renderRamens)
+
+function renderRamens(ramens) {
+  ramens.forEach(renderRamen);
 }
 
-const displayRamens = () => {
-  // Add code
-};
+function renderRamen(ramen) {
+  const ramenMenuDiv = document.getElementById('ramen-menu');
+  
+  const ramenImage = document.createElement('img')
+  ramenImage.src = ramen.image
+  ramenMenuDiv.append(ramenImage)
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  ramenImage.addEventListener("click", (e) => renderDetails(ramen))
 }
 
-main()
+function renderDetails(ramen) {
+  const detailImage = el("detail-image")
+  const ramenName = el("ramen-name")
+  const restaurantName = el("restaurant-name")
+  const ratingDisplay = el("rating-display")
+  const commentDisplay = el("comment-display")
 
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+  detailImage.src = ramen.image;
+  detailImage.alt = ramen.name;
+  ramenName.textContent = ramen.name
+  restaurantName.textContent = ramen.restaurant;
+  ratingDisplay.textContent = ramen.rating;
+  commentDisplay.textContent = ramen.comment;
+}
+
+
+function createNewRamen(e) {
+  e.preventDefault()
+  console.log(e.target.name.value)
+  const newRamen = {
+    name: e.target.name.value,
+    rating: e.target.rating.value,
+    restaurant: e.target.restaurant.value,
+    comment: e.target['new-comment'].value,
+    image: e.target.image.value,
+  };
+  renderRamen(newRamen)
+}
+
+
+function el(elementName) {
+  return document.getElementById(elementName)
+}
+
